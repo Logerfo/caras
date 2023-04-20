b67b3c6c9dd2a280ea0b183c053dc99cdc719b9b
Bruno Logerfo <bclogerfo@hotmail.com>
2023-04-20 15:28:55 -0300

    new limit

--- scripts/decrypt.js
+++ scripts/decrypt.js
@@ -1,6 +1,6 @@
 $(document).ready(async function () {
   const password = prompt("Enter in the password");
-  const maxLength = 4;
+  const maxLength = 36;
   var randomNum = Math.floor(Math.random() * maxLength + 1);
   document.getElementById("myself").src = await decrypt(randomNum, password);
   for (let i = 1; i <= maxLength; i++) {
